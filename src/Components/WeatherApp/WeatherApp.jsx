c56d1af61a449bdfc828081dc50d4d0492498a50
wongzc <52870701+wongzc@users.noreply.github.com>
2023-10-04 20:29:43 +0800

    updated js script and api from openweather

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -9,13 +9,34 @@ import humidity_icon from '../Assets/humidity.png';
 import rain_icon from '../Assets/rain.png';
 import snow_icon from '../Assets/snow.png';
 import wind_icon from '../Assets/wind.png';
+const config = require('./config.json');
 
 const WeatherApp = () => {
+  const apiKey = config.apiKey;
+
+  const search =async()=> {
+    const element =document.getElementsByClassName("cityInput");
+    if(element[0].value==="")
+    {return 0;}
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${apiKey}`;
+
+    let response = await fetch(url);
+    let data = await response.json();
+    const humidity =document.getElementsByClassName("humidity-percent");
+    const wind =document.getElementsByClassName("wind-rate");
+    const temperature =document.getElementsByClassName("weather-temp");
+    const location =document.getElementsByClassName("weather-location");
+
+    humidity[0].innerHTML = data.main.humidity;
+    wind[0].innerHTML = data.wind.speed;
+    temperature[0].innerHTML = data.main.temp;
+    location[0].innerHTML = data.name;
+  }
   return (
     <div className='container'>
       <div className="top-bar">
         <input type="text" className="cityInput" placeholder="search"/>
-        <div className="search-icon">
+        <div className="search-icon" onClick={()=>search()}>
             <img src={search_icon} alt="" />
         </div>
       </div>
@@ -35,7 +56,7 @@ const WeatherApp = () => {
         <div className="element">
             <img src={wind_icon} alt="" className="icon"/>
             <div className="data">
-                <div className="humidity-percent">18 km/h</div>
+                <div className="wind-rate">18 km/h</div>
                 <div className="text">Wind Speed</div>
             </div>
         </div>
